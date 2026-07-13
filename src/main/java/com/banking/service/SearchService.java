27cd112e75e21eeb3acf29211e5acd7e2c51b5c5
Joy-ConScion <chris.torres2001@outlook.com>
2026-07-12 23:50:14 -0500

    Added more skeletal outline for controller

--- src/main/java/com/banking/service/SearchService.java
+++ src/main/java/com/banking/service/SearchService.java
@@ -1,7 +1,22 @@
 package com.banking.service;
 
+import com.banking.models.Search;
+import com.banking.repository.SearchRepository;
+
+import java.util.List;
+
 public class SearchService {
 
+    private SearchRepository searchRepository;
 
+    public SearchService(SearchRepository searchRepository){
+        this.searchRepository = searchRepository;
+    }
+
+    public List<Search> getAllSearches(){
+        return searchRepository.findAll();
+    }
+
+    public List<Search> /*Make one to get singular and another to delete*/
 
 }
