983108ee75ab51eb9d0715f48d84877c9c7da1ab
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-07-13 12:50:20 -0500

    add vendor model

--- src/main/java/com/banking/model/Vendor.java
+++ src/main/java/com/banking/model/Vendor.java
@@ -1,4 +1,42 @@
 package com.banking.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vendors")
 public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255)
+    private String name;
+
+    @Column(length = 100)
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
