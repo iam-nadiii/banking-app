afff4601a972f12df6b4f9bebadaa7b2c0fe77a8
Joy-ConScion <chris.torres2001@outlook.com>
2026-07-12 23:39:17 -0500

    Added skeletal outline for controller

--- src/main/java/com/banking/controllers/SearchController.java
+++ src/main/java/com/banking/controllers/SearchController.java
@@ -1,16 +1,33 @@
 package com.banking.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.banking.models.Search;
+import com.banking.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("searches")
 public class SearchController {
 
+    /*Are we gonna do authorization roles?*/
+
+    @Autowired
     private final SearchService searchService;
 
+    @GetMapping("")
+    public List<Search> search(@RequestParam(name="idk", required = false) Integer idk,
+                                @RequestParam(repeat),
+)
 
+        public Search getById(){
 
+        }
+
+        public ResponseEntity deleteSearch(){
+
+        }
 }
