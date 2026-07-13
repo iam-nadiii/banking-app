63646189cf53b4de50553d2df79c51269e2a6d9a
Joy-ConScion <chris.torres2001@outlook.com>
2026-07-13 13:36:12 -0500

    Pulled main into this branch to view current modifications and know how to proceed.

--- src/main/java/com/banking/service/SearchService.java
+++ src/main/java/com/banking/service/SearchService.java
@@ -2,10 +2,12 @@ package com.banking.service;
 
 import com.banking.models.Search;
 import com.banking.repository.SearchRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class SearchService {
 
     private SearchRepository searchRepository;
