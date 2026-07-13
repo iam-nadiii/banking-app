7914408298d809a972e5193ac73a4ca488dfec74
Joy-ConScion <chris.torres2001@outlook.com>
2026-07-13 14:12:04 -0500

    Status check

--- src/main/java/com/banking/service/SearchService.java
+++ src/main/java/com/banking/service/SearchService.java
@@ -1,5 +1,7 @@
 package com.banking.service;
 
+import com.banking.exception.InvalidInputException;
+import com.banking.exception.ResourceNotFoundException;
 import com.banking.models.Search;
 import com.banking.repository.SearchRepository;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,7 @@ import java.util.Optional;
 @Service
 public class SearchService {
 
-    private SearchRepository searchRepository;
+    private final SearchRepository searchRepository;
 
     public SearchService(SearchRepository searchRepository) {
         this.searchRepository = searchRepository;
@@ -21,14 +23,31 @@ public class SearchService {
     }
 
     public Optional<Search> findById(Long Id) {
-        return searchRepository.findById(Id);
+
+        if (Id <= 0){
+            throw new InvalidInputException("Search ID Invalid (Hint: Number must be positive)");
+        }
+        return Optional.of(searchRepository.findById(Id)
+                .orElseThrow(() -> new ResourceNotFoundException("Search not found with this id: " + Id)));
     }
 
     public Optional<Search> findByVendor(String vendor) {
+        if(vendor==null){
+            throw new InvalidInputException("Transaction type cannot be null");
+        }
         return searchRepository.findByVendor(vendor);
     }
 
     public Search create(Search search) {
+
+        if(search.getId() == null){
+        throw new InvalidInputException("Search ID cannot be empty");
+        }
+
+        if(search.getSearchDate() == null){
+        throw new InvalidInputException("Search date cannot be empty");
+        }
+
         return searchRepository.save(search);
     }
 
